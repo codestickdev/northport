078680c897511a70c6a91fbf1844af11b1c5327f
codestickdev <piotrdevv@gmail.com>
2022-02-24 21:23:28 +0100

    New footer

--- footer.php
+++ footer.php
@@ -13,31 +13,28 @@
 
 	<footer class="siteFooter">
 		<div class="siteFooter__wrap container">
-			<img src="<?php echo get_template_directory_uri() . '/images/logo_footer.png'; ?>" class="siteFooter__logo"/>
-			<div class="siteFooter__image">
-				<img src="<?php echo get_template_directory_uri() . '/images/logo_footer.png'; ?>" class="siteFooter__logo--mobile"/>
-				<img src="<?php echo get_template_directory_uri() . '/images/footer_bg.png'; ?>" class="view"/>
+			<div class="siteFooter__logo">
+				<img src="<?php echo get_template_directory_uri() . '/images/logo_new.svg'; ?>"/>
 			</div>
 			<div class="siteFooter__info">
-				<div class="wrap">
+				<a href="mailto:office@montecarlo.com.pl" class="siteFooter__icon">
+					<img src="<?php echo get_template_directory_uri() . '/images/footer/footer_mail.svg'; ?>"/>
+					<p>office@montecarlo.com.pl</p>
+				</a>
+				<a href="https://bit.ly/3vefmqU" target="_blank" class="siteFooter__icon">
+					<img src="<?php echo get_template_directory_uri() . '/images/footer/footer_location.svg'; ?>"/>
 					<h2>Biuro sprzedaży</h2>
-					<p>Zapraszamy do umówienia się na<br/>wizytę w naszym biurze sprzedaży</p>
-					<p class="address">ul. Mińska 25C/U5<br>03-808 Warszawa</p>
-					<a href="tel:+48508913789">+48 508 913 789</a>
-					<div class="social">
-						<a href="#" target="_blank">
-							<img src="<?php echo get_template_directory_uri() . '/images/social/social_ig_white.svg'; ?>"/>
-						</a>
-						<a href="#" target="_blank">
-							<img src="<?php echo get_template_directory_uri() . '/images/social/social_fb_white.svg'; ?>"/>
-						</a>
-					</div>
-				</div>
+					<p>ul. Mińska 25C/U5<br>03-808 Warszawa</p>
+				</a>
+				<a href="tel:+48508913789" class="siteFooter__icon">
+					<img src="<?php echo get_template_directory_uri() . '/images/footer/footer_phone.svg'; ?>"/>
+					<p>+48 508 913 789</p>
+				</a>
 			</div>
 		</div>
 	</footer>
 	<div class="loader loader--active">
-		<img src="<?php echo get_template_directory_uri() . '/images/logo_footer.png'; ?>"/>
+		<img src="<?php echo get_template_directory_uri() . '/images/logo_new.svg'; ?>"/>
 	</div>
 </div><!-- #page -->
 
